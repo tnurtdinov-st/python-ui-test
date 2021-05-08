fb66f698c84b9d3ff78e301463f4209107f1bf1e
tnurt <blf232@gmail.com>
2021-05-08 17:31:59 +0300

    Урок 8 задание 24

--- conftest.py
+++ conftest.py
@@ -1,8 +1,34 @@
 import pytest
 from fixture.application import Application
+import importlib
+import os
+import pandas as pd
+import numpy as np
+
 
 @pytest.fixture(scope="session")
 def app(request):
     fixture = Application("C:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("excel_"):
+            testdata = load_from_excel(fixture[6:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_excel(file):
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data\\%s.xlsx" % file)
+    raw = pd.read_excel(file, index_col=None, na_values=['NA'], usecols = "A,C:AA").values
+    res = np.array(raw).tolist()
+    list =[]
+    for string in res:
+        tmp=str(string)
+        new_string = tmp.replace("[", "").replace("]", "").replace("'", "")
+        list.append(new_string)
+    return list
+
+
+
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,8 @@
 
-def test_add_group(app):
+def test_add_group(app, excel_data):
+    group = excel_data
     old_list = app.groups.get_group_list()
-    app.groups.add_new_group("my group")
+    app.groups.add_new_group(group)
     new_list = app.groups.get_group_list()
-    old_list.append("my group")
+    old_list.append(group)
     assert sorted(old_list) == sorted(new_list)
